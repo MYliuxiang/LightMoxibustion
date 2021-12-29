dcc747d998c7bc550a73fa39733bd616183832ab
fly <flyliu@tvpc44.tenvine.com>
2021-12-29 16:25:44 +0800

    2

---
+++ LightMoxibustion/Home/Views/DisconnectedAlert.h
@@ -0,0 +1,16 @@
+//
+//  DisconnectedAlert.h
+//  LightMoxibustion
+//
+//  Created by flyliu on 2021/12/27.
+//
+
+#import "LxCustomAlert.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DisconnectedAlert : LxCustomAlert
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LightMoxibustion/Home/Views/DisconnectedAlert.m
@@ -0,0 +1,20 @@
+//
+//  DisconnectedAlert.m
+//  LightMoxibustion
+//
+//  Created by flyliu on 2021/12/27.
+//
+
+#import "DisconnectedAlert.h"
+
+@implementation DisconnectedAlert
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
